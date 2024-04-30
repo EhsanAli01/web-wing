dcc6aabdc501a944ca47c034506f8c6e94d9b474
Ehsan Ali <148131941+EhsanAli01@users.noreply.github.com>
2024-04-30 22:33:32 +0500

    Update Post.jsx

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -26,7 +26,7 @@ const Post = () => {
     form.append('imageTile' , imageTitle);
     form.append('imageDescription' , imageDescription);
 
-    axios.post('https://backend-phi-red.vercel.app/posts/createNewPost/' + localStorage.getItem('userId') , form)
+    axios.post('http://backend-phi-red.vercel.app/posts/createNewPost/' + localStorage.getItem('userId') , form)
     .then(result => {
       console.log(result);
       setloading(false);
